72d54cd0c1c65cee15c63bb657ddbc8802360536
ben <wbpiggot@gmail.com>
2020-11-30 13:52:40 -0800

    first commit

---
+++ src/components/route-login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const RouteLogin = () => {
+  return (
+    <>
+      <h1>Log In or Sign up</h1>
+      <button>Log In</button>
+    </>
+  )
+};
+
+export default RouteLogin;
